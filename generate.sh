4c1af8b097fe2d78e058b4e05b7be24d9de3d730
Wade Wegner <wade.wegner@gmail.com>
2018-01-07 13:26:38 -0800

    Added argument completions for all commands

--- generate.sh
+++ generate.sh
@@ -4,6 +4,7 @@ export FORCE_SHOW_SPINNER=
 
 # create the commands-list.json file
 sfdx force:doc:commands:list --json | jq -r .result > commands-list.json
+sfdx force:doc:commands:display --json | jq '.result[] | select((.command) != null)' > commands-display.json
 
 completion=""
 completion+="#compdef sfdx"
@@ -11,7 +12,7 @@ completion+="#compdef sfdx"
 # header
 completion+="\n"
 completion+="\n# DESCRIPTION: Zsh completion script for the Salesforce CLI"
-completion+="\n# AUTHOR: Wade Wegner (@WadeWegner)"
+completion+="\n# AUTHOR: Wade Wegner"
 completion+="\n# REPO: https://github.com/wadewegner/salesforce-cli-zsh-completion"
 completion+="\n# LICENSE: https://github.com/wadewegner/salesforce-cli-zsh-completion/blob/master/LICENSE"
 completion+="\n"
@@ -20,12 +21,10 @@ completion+="\n"
 completion+="\n_1st_arguments=("
 
 commands=""
-
 while read name description
 do
   name="$(echo $name | sed -e 's/:/\\:/g')"
   commands="${commands}\n\t\"$name\":\"$description\""
-
 done <<< "$(jq -r 'to_entries[] | "\(.value.name)\t\(.value.description)"' commands-list.json)"
 
 completion+=$commands
@@ -38,5 +37,53 @@ completion+="\n  _describe -t commands \"sfdx command\" _1st_arguments"
 completion+="\n  return"
 completion+="\nfi"
 completion+="\n"
+
+completion+="\nlocal -a _command_args"
+completion+="\ncase \"\$words[1]\" in"
+
+while read delimitedFullCommand
+do
+
+  fullCommandArray=($delimitedFullCommand)
+  topic=${fullCommandArray[0]}
+  command=${fullCommandArray[1]}
+  fullCommand=$topic:$command
+
+  completion+="\n  $fullCommand)"
+  completion+="\n    _command_args=("
+
+  delimitedFlags=$(jq -r '. | select((.command == "'$command'") and (.topic == "'$topic'")) | .flags | .[] | .name + "\t" + .description + "\t" + .char' commands-display.json)  #echo "jqFlags: " $delimitedFlags
+  
+  IFS=$'\n'
+  flagArray=($delimitedFlags)
+  IFS=$'\t'
+  for flagArrayRow in "${flagArray[@]}"
+  do
+    flagArray2=($flagArrayRow)
+
+    flagName=${flagArray2[0]}
+    flagDescription=${flagArray2[1]}
+    flagChar=${flagArray2[2]}
+
+    if [ "$flagChar" != "" ]
+    then
+      completion+="\n      '(-"$flagChar"|--"$flagName")'{-"$flagChar",--"$flagName"}'["$flagDescription"]' \\\\"
+    else
+      completion+="\n      '(--"$flagName")--"$flagName"["$flagDescription"]' \\\\"
+    fi
+
+  done
+  IFS=' '
+
+  completion+="\n    )"
+  completion+="\n    ;;"
+  
+done <<< "$(jq -r '"\(.topic) \(.command)"' commands-display.json)"
+
+completion+="\n  esac"
+completion+="\n"
+completion+="\n_arguments \\\\"
+completion+="\n  \$_command_args \\\\"
+completion+="\n  && return 0"
 
 echo $completion > _sfdx
